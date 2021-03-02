a3f56db8552ebab486fdb0b2baaa92173c461eda
Jack <jack.branthwaite@gmail.com>
2021-03-03 10:08:51 +1300

    Can now update the database from posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,9 +3,8 @@ const { restart } = require("nodemon");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {});
-
-router.get("/specific", async (req, res) => {
+//Gets back all the posts
+router.get("/", async (req, res) => {
   try {
     const post = await Post.find();
     res.json(post);
@@ -14,6 +13,7 @@ router.get("/specific", async (req, res) => {
   }
 });
 
+//Submits a post
 router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -24,6 +24,39 @@ router.post("/", async (req, res) => {
     const savedPost = await post.save();
     res.json(savedPost);
   } catch {
+    res.json({ message: err });
+  }
+});
+
+//Gets a specific post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Delete a specific post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.deleteOne({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Update a post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatePost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatePost);
+  } catch (err) {
     res.json({ message: err });
   }
 });
