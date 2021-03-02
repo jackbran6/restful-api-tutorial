5dc1838519204773f209f75cf9e5804cba4b91ba
Jack <jack.branthwaite@gmail.com>
2021-03-02 21:39:08 +1300

    mongoose added

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//Middleware - function that executes when routes are run
+app.use("/posts", () => {
+  console.log("This is middleware running");
+});
+
 //Routes
 app.get("/", (req, res) => {
   res.send("we are home");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("we are posts");
 });
 
 //Boot up the server
