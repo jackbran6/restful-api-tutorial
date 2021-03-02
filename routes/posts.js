3f0c3a0c48b6c436c22e8fb3ba42ea5688ede02f
Jack <jack.branthwaite@gmail.com>
2021-03-03 09:03:39 +1300

    Data can now be retrieved from the database

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const { restart } = require("nodemon");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("we are posts");
+router.get("/", (req, res) => {});
+
+router.get("/specific", async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.get("/specific", (req, res) => {
-  res.send("we are specific posts");
-});
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
